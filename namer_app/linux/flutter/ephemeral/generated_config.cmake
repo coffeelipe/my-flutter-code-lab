56df7696a3761327917f386a98405792e8c736d6
Felipe Pontes <fpontes321@gmail.com>
2025-01-29 08:30:02 -0300

    build files added to repository

---
+++ namer_app/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/coffeelipe/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/coffeelipe/Projects/personal/flutter-codelab/namer_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/coffeelipe/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/coffeelipe/Projects/personal/flutter-codelab/namer_app"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/coffeelipe/Projects/personal/flutter-codelab/namer_app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
